48f716e8c0ef706c29e4cc1bd8052699e0be1c0f
StefanDellamore <dellamoreste.info@gmail.com>
2024-01-17 16:52:59 +0100

    Completamento esercizio

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+//1)importazione
+import AppHeader from './components/AppHeader.vue';
+export default {
+    data() {
+        return { count: 0 };
+    },
+    //2)dichiarazione componenti
+    components: {
+        AppHeader
+    }
+};
+</script>
+
+<template>
+    <div>
+        <!--3)dichiarazione componenti--> 
+        <AppHeader/>
+    </div>
+</template>
+
+<style scoped>
+    /* */
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,23 @@
+<script>
+// my-component.js
+export default {
+    data() {
+        return { title: 'La mia prima app con Vite!' };
+    },
+};
+</script>
+
+<template>
+    <div>
+        <h1>
+            {{ title }}
+        </h1>
+    </div>
+</template>
+
+<style scoped>
+    h1{
+        color: red;
+        text-align: center;
+    }
+</style>
